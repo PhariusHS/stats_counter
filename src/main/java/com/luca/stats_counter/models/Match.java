c577acaa1ebe8dd62065fc24c987faf7e51bc711
Luca Ponteprimo <Lucaponteprimo04@gmail.com>
2025-04-02 10:42:03 +0200

    fixed: deleteMatchService

--- src/main/java/com/luca/stats_counter/models/Match.java
+++ src/main/java/com/luca/stats_counter/models/Match.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -43,7 +44,7 @@ public class Match {
     @Transient
     @JsonIgnore
     @JsonManagedReference(value = "match_statistics_reference")
-    @OneToMany(mappedBy = "match", cascade = CascadeType.ALL, orphanRemoval=true)
+    @OneToMany(mappedBy = "match", cascade = CascadeType.ALL, orphanRemoval=true, fetch = FetchType.EAGER )
     private List<Statistics> matchStatistics;
 
     
--- src/main/java/com/luca/stats_counter/services/matchServices/DeleteMatchService.java
+++ src/main/java/com/luca/stats_counter/services/matchServices/DeleteMatchService.java
@@ -1,5 +1,6 @@
 package com.luca.stats_counter.services.matchServices;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
@@ -8,7 +9,9 @@ import org.springframework.stereotype.Service;
 
 import com.luca.stats_counter.Command;
 import com.luca.stats_counter.models.Match;
+import com.luca.stats_counter.models.Statistics;
 import com.luca.stats_counter.repositories.MatchRepository;
+import com.luca.stats_counter.repositories.StatisticsRepository;
 
 import jakarta.persistence.EntityNotFoundException;
 
@@ -16,14 +19,21 @@ import jakarta.persistence.EntityNotFoundException;
 public class DeleteMatchService implements Command<Long, Void>{
 
     private final MatchRepository matchRepository;
-    public DeleteMatchService(MatchRepository matchRepository){
+    private final StatisticsRepository statisticsRepository;
+    public DeleteMatchService(MatchRepository matchRepository, StatisticsRepository statisticsRepository){
         this.matchRepository = matchRepository;
+        this.statisticsRepository = statisticsRepository;
     } // Immutable DI;
 
     @Override
     public ResponseEntity<Void> execute(Long matchId) {
         Optional<Match> foundedMatch = matchRepository.findById(matchId);
         if(foundedMatch.isPresent()){//Verifies if the match exists
+            List<Statistics> matchStatistics = statisticsRepository.findByMatch(foundedMatch.get());
+            for(Statistics i : matchStatistics){
+                statisticsRepository.deleteById(i.getId());
+            } // delete statistics before deleting the Match
+            
             matchRepository.deleteById(matchId);
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         }
