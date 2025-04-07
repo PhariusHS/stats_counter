7cb1767d52bb13fd9c5c48e7169716357d186d5d
Luca Ponteprimo <Lucaponteprimo04@gmail.com>
2025-04-07 13:23:50 +0200

    proving cors

--- src/main/java/com/luca/stats_counter/controllers/TeamController.java
+++ src/main/java/com/luca/stats_counter/controllers/TeamController.java
@@ -15,6 +15,7 @@ import com.luca.stats_counter.services.teamServices.UpdateTeamService;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/team")
 public class TeamController {
